ccf6c2df07e9c48850977b8a4f024e6c1c2c3f8b
unknown <aryaelumallai@gmail.com>
2026-03-03 21:06:17 +0530

    Added UC6

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,26 +1,8 @@
 public class OopsBannerApp{
     public static void main(String[] args) {
 
-        String[] letterO = buildO();
-        String[] letterP = buildP();
-        String[] letterS = buildS();
-
-        String[] banner = new String[7];
-
-        for (int i = 0; i < 7; i++) {
-            banner[i] = letterO[i] + "   "
-                      + letterO[i] + "   "
-                      + letterP[i] + "   "
-                      + letterS[i];
-        }
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
-    }
-
-    private static String[] buildO() {
-        return new String[]{
+        // Create character mappings
+        CharacterPatternMap letterO = new CharacterPatternMap('O', new String[]{
                 "   ***   ",
                 "  **   **  ",
                 " **     ** ",
@@ -28,11 +10,9 @@ public class OopsBannerApp{
                 " **     ** ",
                 "  **   **  ",
                 "   ***   "
-        };
-    }
+        });
 
-    private static String[] buildP() {
-        return new String[]{
+        CharacterPatternMap letterP = new CharacterPatternMap('P', new String[]{
                 " ******  ",
                 " **    ** ",
                 " **    ** ",
@@ -40,11 +20,9 @@ public class OopsBannerApp{
                 " **       ",
                 " **       ",
                 " **       "
-        };
-    }
+        });
 
-    private static String[] buildS() {
-        return new String[]{
+        CharacterPatternMap letterS = new CharacterPatternMap('S', new String[]{
                 "  ****** ",
                 " **      ",
                 " **      ",
@@ -52,6 +30,42 @@ public class OopsBannerApp{
                 "      ** ",
                 "      ** ",
                 " ******  "
+        });
+
+        // Word to render
+        CharacterPatternMap[] word = {
+                letterO, letterO, letterP, letterS
         };
+
+        // Render banner
+        for (int i = 0; i < 7; i++) {
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (CharacterPatternMap cp : word) {
+                lineBuilder.append(cp.getPattern()[i]).append("   ");
+            }
+
+            System.out.println(lineBuilder.toString());
+        }
+    }
+
+    // Static Inner Class
+    static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
 }
